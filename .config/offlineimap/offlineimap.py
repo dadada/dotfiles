5e03a1cc56584a8b53ee5de874ac28a212216960
Tim Schubert <mail@timschubert.net>
2019-05-29 21:28:54 +0200

    Exclude some mailboxes from beeing synced

--- .config/offlineimap/offlineimap.py
+++ .config/offlineimap/offlineimap.py
@@ -1,6 +1,16 @@
 #! /usr/bin/env python2
 from subprocess import check_output
 
+folder_blacklist = [
+        '+timschubert.net/Trash',
+        '+timschubert.net/feeds',
+        '+timschubert.net/Junk',
+        '+timschubert.net/DeltaChat'
+        ]
 
 def get_password(account):
     return check_output("pass " + account, shell=True).splitlines()[0]
+
+
+def mbnames_folderfilter(accountname, foldername):
+    return foldername not in folder_blacklist
