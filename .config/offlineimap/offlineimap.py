00b904c400ca42507b859f24d7b1110869d601cd
Tim Schubert <mail@timschubert.net>
2019-07-16 16:03:30 +0200

    Update email filtering

--- .config/offlineimap/offlineimap.py
+++ .config/offlineimap/offlineimap.py
@@ -1,16 +1,10 @@
 #! /usr/bin/env python2
 from subprocess import check_output
 
-folder_blacklist = [
-        '+timschubert.net/Trash',
-        '+timschubert.net/feeds',
-        '+timschubert.net/Junk',
-        '+timschubert.net/DeltaChat'
-        ]
 
 def get_password(account):
     return check_output("pass " + account, shell=True).splitlines()[0]
 
 
-def mbnames_folderfilter(accountname, foldername):
-    return foldername not in folder_blacklist
+def folderfilter(foldername):
+    return foldername not in ['Spam', 'feeds', 'Junk']
